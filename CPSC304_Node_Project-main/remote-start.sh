223b19c9dff6b7257b56a320908569d8c0ef25d1
Nancy <yma60@student.ubc.ca>
2025-08-03 13:21:12 -0700

    for setup

--- CPSC304_Node_Project-main/remote-start.sh
+++ CPSC304_Node_Project-main/remote-start.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
 
 # Set Oracle environment
-if [ -d /opt/oracle/instantclient_19_8 ]; then
-    export ORACLE_HOME=/opt/oracle/instantclient_19_8
+# if [ -d /opt/oracle/instantclient_19_8 ]; then
+#     export ORACLE_HOME=/opt/oracle/instantclient_19_8
+#     export LD_LIBRARY_PATH=$ORACLE_HOME
+# elif [ -d /usr/lib/oracle/19.6/client64/lib ]; then
+#     export ORACLE_HOME=/usr/lib/oracle/19.6/client64
+#     # 19.* libraries will be already configured by ldconfig
+#     #export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+# elif [ -d /usr/lib/oracle/12.2/client64/lib ]; then
+#     export ORACLE_HOME=/usr/lib/oracle/12.2/client64
+#     export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+# elif [ -d /home/o/oracle ]; then     # <-- ADD THIS
+#     export ORACLE_HOME=/home/o/oracle
+#     export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+# else
+#     echo "Oracle not found..."
+#     exit 1
+# fi
+echo "Checking for Oracle installations..."
+
+if [ -d /opt/oracle/instantclient_23_7 ]; then
+    echo "Found at /opt/oracle/instantclient_23_7"
+    export ORACLE_HOME=/opt/oracle/instantclient_23_7
     export LD_LIBRARY_PATH=$ORACLE_HOME
 elif [ -d /usr/lib/oracle/19.6/client64/lib ]; then
+    echo "Found at /usr/lib/oracle/19.6/client64/lib"
     export ORACLE_HOME=/usr/lib/oracle/19.6/client64
-    # 19.* libraries will be already configured by ldconfig
-    #export LD_LIBRARY_PATH=$ORACLE_HOME/lib
 elif [ -d /usr/lib/oracle/12.2/client64/lib ]; then
+    echo "Found at /usr/lib/oracle/12.2/client64/lib"
     export ORACLE_HOME=/usr/lib/oracle/12.2/client64
     export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+elif [ -d /home/o/oracle ]; then
+    echo "Found at /home/o/oracle"
+    export ORACLE_HOME=/home/o/oracle
+    export LD_LIBRARY_PATH=$ORACLE_HOME/lib
 else
-    echo "Oracle not found..."
+    echo "Oracle not found... exiting."
     exit 1
 fi
-
 
 # Configure the shared Node library on the undergrad server.
 export NODE_PATH=/cs/local/generic/lib/cs304/node_modules
